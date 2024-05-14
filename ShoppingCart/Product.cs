f966d4a01cc21c9fb197b888e564069f244e8a59
Sonja Perković <soperkov@vsite.hr>
2024-05-14 19:30:00 +0200

    test commit

---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ShoppingCart
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+
+        public Product(int id, string name, double price)
+        {
+            _id = id;
+            _name = name;
+            _price = price;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+    }
+}
---
+++ ShoppingCart/Stock.cs
@@ -0,0 +1,28 @@
+﻿namespace ShoppingCart
+{
+    public class Stock
+    {
+        private Dictionary<int, uint> products;
+
+        public Stock()
+        {
+            products = new Dictionary<int, uint>();
+        }
+        public void AddToStock(Product product, uint numberOfItems = 1)
+        {
+            int id = product.GetId();
+            products[id] += numberOfItems;
+        }
+
+        public bool RemoveProduct(int id)
+        {
+            if (products.ContainsKey(id))
+            {
+                products[id]--;
+                if (products[id] == 0) products.Remove(id);
+                return true;
+            }
+            return false;
+        }
+    }
+}
