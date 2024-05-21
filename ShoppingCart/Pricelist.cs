eb409aa362adede91a300ac8963840bb19b217f7
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-21 08:57:11 +0200

    Implemented classes

---
+++ ShoppingCart/Pricelist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class Pricelist
+    {
+        private readonly Dictionary<Product, decimal> _pricelist = [];
+
+        public Dictionary<Product, decimal> GetProducts()
+        {
+            return new Dictionary<Product, decimal>(_pricelist);
+        }
+        public void AddProduct(Product product, decimal price)
+        {
+            if (!_pricelist.TryAdd(product, price))
+            {
+                _pricelist[product] = price;
+            }
+        }
+
+        public decimal GetPrice(Product product)
+        {
+            return _pricelist[product];
+        }
+    }
+}
---
+++ ShoppingCart/Store.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class Store
+    {
+        private readonly Pricelist _pricelist;
+        private readonly Warehouse _warehouse;
+
+        public Store(Pricelist pricelist, Warehouse warehouse)
+        {
+            _pricelist = pricelist;
+            _warehouse = warehouse;
+        }
+
+        public Dictionary<Product, decimal> GetProducts()
+        {
+            return _pricelist.GetProducts();
+        }
+        public Product GetProductById(int id)
+        {
+            return _pricelist.GetProducts().FirstOrDefault(x => x.Key.Id == id).Key;
+        }
+
+        public void AddStock(Product product, int quantity)
+        {
+            _warehouse.AddProduct(product, quantity);
+        }
+        public void RemoveStock(Product product, int quantity)
+        {
+            _warehouse.RemoveProduct(product, quantity);
+        }
+
+        public bool IsProductAvailable(Product product, int quantity)
+        {
+            return _warehouse.IsAvailable(product, quantity);
+        }
+    }
+}
