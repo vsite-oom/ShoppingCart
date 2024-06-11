7fb4cb6baf24551bfa7bdd7b2dbec2bd2d94c348
Đuro Rukavina <djrukavina@vsite.hr>
2024-06-11 14:41:25 +0200

    Added Name getter

--- ShoppingCart/Product.cs
+++ ShoppingCart/Product.cs
@@ -5,6 +5,7 @@
         private readonly int _id;
         private readonly string _name;
         private readonly double _price;
+        public string Name => _name;
         public double GetPrice => _price;
 
         private static int _instanceId = 0;
