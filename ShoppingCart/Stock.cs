1ae70064143c9977acffa9479d723ebd2e3180d7
Đuro Rukavina <djrukavina@vsite.hr>
2024-06-11 12:22:11 +0200

    Added class Stock

---
+++ ShoppingCart/Stock.cs
@@ -0,0 +1,27 @@
+﻿namespace ShoppingCart
+{
+    public class Stock
+    {
+        private Dictionary<Product, int> _products = new Dictionary<Product, int>();
+        public IReadOnlyDictionary<Product, int> Products { get { return _products; } }
+
+        public void AddStock(Product product, int quantity)
+        {
+            if (_products.ContainsKey(product))
+            {
+                _products[product] += quantity;
+            }
+            else
+            {
+                _products.Add(product, quantity);
+            }
+        }
+        public void RemoveStock(Product product, int quantity)
+        {
+            if (_products.ContainsKey(product) && _products[product] >= quantity)
+            {
+                _products[product] -= quantity;
+            }
+        }
+    }
+}
