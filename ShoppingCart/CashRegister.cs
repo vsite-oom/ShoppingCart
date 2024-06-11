913a6f9d7f0b729a659257e1ea3a967620e8be95
Đuro Rukavina <djrukavina@vsite.hr>
2024-06-11 13:32:25 +0200

    Added class CashRegister

---
+++ ShoppingCart/CashRegister.cs
@@ -0,0 +1,26 @@
+﻿namespace ShoppingCart
+{
+    public class CashRegister
+    {
+        private int _id;
+        private double _amount;
+
+        private static int _instanceID = 0;
+
+        public CashRegister()
+        {
+            _id = ++_instanceID;
+            _amount = 0;
+        }
+
+        public double CalculateAmount(Cart cart)
+        {
+            var products = cart.GetProducts();
+            foreach (var product in products)
+            {
+                _amount += product.Key.GetPrice() * product.Value;
+            }
+            return _amount;
+        }
+    }
+}
