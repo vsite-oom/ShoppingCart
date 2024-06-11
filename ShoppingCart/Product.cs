88ac666d86b4c7e9c0e30d0c660e74dd7c119d6d
Đuro Rukavina <djrukavina@vsite.hr>
2024-06-11 12:04:55 +0200

    Added class Product

---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ShoppingCart
+{
+    public class Product
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly double _price;
+
+        private static int _instanceId = 0;
+
+        public Product(string name, double price)
+        {
+            _id = ++_instanceId;
+            _name = name;
+            _price = price;
+        }
+
+    }
+}
